df569a4b30bf906aa28c13f4fb42971f8abcc9de
Evgeniy <e.pogrebnyak@gmail.com>
2020-12-27 12:13:49 +0300

    create ssg.csv

--- table.py
+++ table.py
@@ -5,22 +5,9 @@ def make_presentation(df):
     zf['stars'] = zf.stars.divide(1000).round(1)
     zf.index=df.apply(lambda x: md_link(x.name, x.url), result_type='expand', axis=1) 
     del zf['url']
+    zf.columns = ["'000 stars", "Language"]
+    return zf
 
-df = get_dataframe("ssg.yaml")
-print(df)
+df = make_presentation(get_dataframe("ssg.yaml"))
+print(df.to_markdown(tablefmt="github", floatfmt=".1f"))
 
-import altair as alt
-df['stars'] = df.stars.divide(1000).round(1)
-
-ch = alt.Chart(df,
-               title="Static site generators popularity (26.12.2020)",               
-               ).mark_bar().encode(
-     x=alt.X('stars', title="\'000 stars on Github"),
-     y=alt.Y('name', 
-             sort=alt.EncodingSortField(field="stars", order='descending'),
-             title=""),
-     color=alt.Color("lang", 
-                     legend=alt.Legend(title="Language"),
-                     scale=alt.Scale(scheme='tableau10'))
-)
-ch.show()    
