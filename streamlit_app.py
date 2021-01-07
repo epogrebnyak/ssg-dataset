96c21e5d9a576dc02a0df7fabe5f7507238118bd
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-08 00:08:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,17 +17,24 @@ st.header("Static site generators popularity  :thermometer: :star:")
 Static site generators are tools to create blogs, landing pages and documentation.
 """
 
+
 @st.cache
 def get_data():
     df = pd.read_csv(url)
     df["stars"] = df.stars.divide(1000).round(1)
     return df
 
-df = get_data() 
+
+df = get_data()
+
+# FIXME: should be able to reset https://discuss.streamlit.io/t/reset-multiselect-to-default-values-using-a-checkbox/1941
+all_langs = df.lang.unique().tolist()
+langs = st.multiselect("Programming languages", options=all_langs, default=all_langs)
+plot_df = df[df.lang.isin(langs)]
 
 chart = (
     alt.Chart(
-        df,
+        plot_df,
         title="Static site generators popularity",
     )
     .mark_bar()
@@ -48,6 +55,14 @@ chart = (
 
 st.altair_chart(chart, use_container_width=True)
 
+st.header("Discussion")
+
+"""
+[![](https://user-images.githubusercontent.com/9265326/103943179-ae5db400-5142-11eb-90be-111c8c520f93.png)][link]
+
+[link]: https://twitter.com/PogrebnyakE/status/1343105678261555200
+"""
+
 st.subheader("Links")
 st.write("\n".join(f"- [{name}]({url})" for name, url in zip(df.name, df.url)))
 
@@ -55,6 +70,5 @@ st.write("\n".join(f"- [{name}]({url})" for name, url in zip(df.name, df.url)))
 st.subheader("Data")
 st.dataframe(df)
 f"""
-Data collected with [epogrebnyak/ssg](https://github.com/epogrebnyak/ssg). Static CSV file: [ssg.csv]({url})
+Static CSV file: [ssg.csv]({url}) (collected with [epogrebnyak/ssg](https://github.com/epogrebnyak/ssg)).
 """
-
