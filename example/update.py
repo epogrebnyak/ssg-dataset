2f314d5e98dfc15f668a6be2f9a01eed6a05da4b
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-07 22:39:05 +0300

    Create update.py

---
+++ example/update.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from ssg import get_dataframe
+
+data_folder = Path(__name__).parent / "data"
+df = get_dataframe(data_folder / "ssg.yaml")
+df[["stars", "lang", "url"]].to_csv(data_folder / "ssg.csv")
