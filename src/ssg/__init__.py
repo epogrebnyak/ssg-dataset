02e79d5593c9b8833398b9e812191dac94916b1c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-06-19 09:47:57 +0300

    #13 separate three parts in text_yaml_to_csv()

--- src/ssg/__init__.py
+++ src/ssg/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.0"
-
 from .stars import get_dataframe, yaml_to_csv
+
+__version__ = "0.0.0"
--- tests/test_ssg.py
+++ tests/test_ssg.py
@@ -1,5 +1,10 @@
+import datetime
+
+import pandas as pd
+import pytest
+
 from ssg import __version__
-from ssg.stars import Repo, yaml_to_csv, create_all
+from ssg.stars import Repo, create_all, yaml_to_csv
 
 
 def test_version():
@@ -8,9 +13,6 @@ def test_version():
 
 def test_n_forks():
     assert Repo("epogrebnyak/haskell-intro").n_forks() >= 5
-
-
-import pytest
 
 
 class TestFilesBase:
@@ -29,7 +31,7 @@ segmentio/metalsmith:
         self.metadata_path = tmpdir / "metadata.json"
 
 
-class CreateAll(TestFilesBase):
+class Test_create_all(TestFilesBase):
     def test_csv_is_created(self):
         create_all(self.folder)
         assert self.csv_path.exists()
@@ -39,17 +41,7 @@ class CreateAll(TestFilesBase):
         assert self.metadata_path.exists()
 
 
-def test_yaml_to_csv(tmpdir):
-    import datetime
-    import pandas as pd
-
-    yaml_doc = """
-rstudio/bookdown:
-  lang: r
-segmentio/metalsmith:
-  lang: js"""
-    yaml_path = tmpdir / "ssg.yaml"
-    csv_path = tmpdir / "ssg.csv"
+class Test_yaml_to_csv(TestFilesBase):
     columns = [
         "handle",
         "created",
@@ -61,43 +53,48 @@ segmentio/metalsmith:
         "repo_lang",
         "url",
     ]
-    metadata_path = tmpdir / "metadata.json"
-    yaml_path.write_text(yaml_doc, encoding="utf-8")
-    df, _ = yaml_to_csv(str(tmpdir), "ssg.yaml", "ssg.csv")
-    df2 = pd.read_csv(csv_path, parse_dates=["created", "modified"])
-    # test dataframe equals the written csv file
-    pd.testing.assert_frame_equal(
-        df2.set_index("name", drop=False), df[df2.columns], check_dtype=False
-    )
-    # test mutable fileds will only go up
-    assert df.loc["metalsmith", "stars"] >= 7736
-    assert df.loc["bookdown", "stars"] >= 2985
-    assert df.loc["metalsmith", "forks"] >= 667
-    assert df.loc["bookdown", "forks"] >= 1135
-    assert df.loc["metalsmith", "open_issues"] >= 32
-    assert df.loc["bookdown", "open_issues"] >= 175
-    # test the rest of fields contents
-    assert df.drop(columns=["stars", "forks", "open_issues", "modified"]).to_dict() == {
-        "name": {"metalsmith": "metalsmith", "bookdown": "bookdown"},
-        "handle": {
-            "metalsmith": "segmentio/metalsmith",
-            "bookdown": "rstudio/bookdown",
-        },
-        "lang": {"metalsmith": "js", "bookdown": "r"},
-        "exec": {"metalsmith": False, "bookdown": False},
-        "twitter": {"metalsmith": "", "bookdown": ""},
-        "site": {"metalsmith": "", "bookdown": ""},
-        "url": {
-            "metalsmith": "https://github.com/segmentio/metalsmith/",
-            "bookdown": "https://github.com/rstudio/bookdown/",
-        },
-        "created": {
-            "metalsmith": datetime.date(2014, 2, 4),
-            "bookdown": datetime.date(2015, 10, 28),
-        },
-        "homepage": {
-            "metalsmith": "https://metalsmith.io",
-            "bookdown": "https://pkgs.rstudio.com/bookdown/",
-        },
-        "repo_lang": {"metalsmith": "JavaScript", "bookdown": "JavaScript"},
-    }
+
+    def test_we_write_exact_data(self):
+        df, _ = yaml_to_csv(self.folder, "ssg.yaml", "ssg.csv", self.columns)
+        df2 = pd.read_csv(self.csv_path, parse_dates=["created", "modified"])
+        pd.testing.assert_frame_equal(
+            df2.set_index("name", drop=False), df[df2.columns], check_dtype=False
+        )
+
+    def test_dataframe_immutable_data(self):
+        df, _ = yaml_to_csv(self.folder, "ssg.yaml", "ssg.csv", self.columns)
+        assert df.loc["metalsmith", "stars"] >= 7736
+        assert df.loc["bookdown", "stars"] >= 2985
+        assert df.loc["metalsmith", "forks"] >= 667
+        assert df.loc["bookdown", "forks"] >= 1135
+        assert df.loc["metalsmith", "open_issues"] >= 32
+        assert df.loc["bookdown", "open_issues"] >= 175
+
+    def test_dataframe_mutable_data(self):
+        df, _ = yaml_to_csv(self.folder, "ssg.yaml", "ssg.csv", self.columns)
+        assert df.drop(
+            columns=["stars", "forks", "open_issues", "modified"]
+        ).to_dict() == {
+            "name": {"metalsmith": "metalsmith", "bookdown": "bookdown"},
+            "handle": {
+                "metalsmith": "segmentio/metalsmith",
+                "bookdown": "rstudio/bookdown",
+            },
+            "lang": {"metalsmith": "js", "bookdown": "r"},
+            "exec": {"metalsmith": False, "bookdown": False},
+            "twitter": {"metalsmith": "", "bookdown": ""},
+            "site": {"metalsmith": "", "bookdown": ""},
+            "url": {
+                "metalsmith": "https://github.com/segmentio/metalsmith/",
+                "bookdown": "https://github.com/rstudio/bookdown/",
+            },
+            "created": {
+                "metalsmith": datetime.date(2014, 2, 4),
+                "bookdown": datetime.date(2015, 10, 28),
+            },
+            "homepage": {
+                "metalsmith": "https://metalsmith.io",
+                "bookdown": "https://pkgs.rstudio.com/bookdown/",
+            },
+            "repo_lang": {"metalsmith": "JavaScript", "bookdown": "JavaScript"},
+        }
