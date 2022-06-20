a92467d9a0ab5bc4e574508533de23c85067e5ce
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-06-20 14:05:03 +0000

    draft pallette for #9

--- a.py
+++ a.py
@@ -1,13 +1,11 @@
 # %%
-import httpx 
+import requests 
+from ssg.stars import allowed_languages, pretty
 
-r = httpx.get("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json").json()
-r2 = {k.lower(): v["color"] for k, v in r.items()} 
-# %%
-from ssg.stars import allowed_languages
 
-for lang in allowed_languages:
-    print(lang, r2.get(lang))
+def palette(allowed_languages):
+    r = requests.get("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json").json()
+    return {pretty(lang):r[pretty(lang)]["color"] for lang in allowed_languages}
 # %%
 import pandas as pd
 url = ("https://raw.githubusercontent.com/"
