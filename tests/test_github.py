b4278c4004f74e987fd161c267f9ac65e6aa0f44
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-06-22 12:48:28 +0300

    Update test_github.py

--- tests/test_github.py
+++ tests/test_github.py
@@ -8,6 +8,6 @@ def test_get_repo_state_from_handle():
     assert rs.homepage == "https://pkgs.rstudio.com/bookdown/"
     assert rs.created == datetime.date(2015, 10, 28)
     assert rs.modified >= datetime.date(2022, 6, 14)
-    assert rs.stars >= 2985
-    assert rs.forks >= 1135
-    assert isinstance(rs.open_issues, int)
+    assert rs.stars >= 2985 # we assume number of stars does not decrease  
+    assert rs.forks >= 1135 # we assume number of forks also does not decrease 
+    assert isinstance(rs.open_issues, int) # number of issues is known to decrease, so we check only type (integer)
