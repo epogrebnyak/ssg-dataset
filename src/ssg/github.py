9b1723637adce48b9633a3beb6b40c79fa1ef5fd
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-06-23 00:22:27 +0300

    Update github.py

--- src/ssg/github.py
+++ src/ssg/github.py
@@ -68,7 +68,7 @@ def last_modified(handle: str) -> str:
 def date_only(ts: str) -> date:
     """Extract date from *ts* timestamp in YYYY-mm-ddTHH:MM:SSZ format."""
     # must to strip 'Z' first
-    return datetime.fromisoformat(ts.rstrip('Z'))
+    return datetime.fromisoformat(ts.rstrip('Z')).date()
 
 
 
