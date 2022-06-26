8596ae6b1c3090dc9bea4ccb6324344832f06ed8
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2022-06-26 16:43:41 +0000

    create badge.md for #16

--- badge.py
+++ badge.py
@@ -1,12 +1,36 @@
+from dataclasses import dataclass 
 from pybadges import badge
 from typing import Optional
 
+@dataclass
+class Badge:
+    left_text: str
+    right_text: str
+    right_color: str
+    left_color: str = '#555'
 
-def badge_generate(left_text: str,
+    def image(self):
+        return badge(
+        left_text = self.left_text,
+        right_text = self.right_text,
+        left_color = self.left_color,
+        right_color = self.right_color,
+    )
+
+    def image_with_link(self, url):
+        return badge(
+        left_text = self.left_text,
+        right_text = self.right_text,
+        right_link = url,
+        left_link = url,
+        left_color = self.left_color,
+        right_color = self.right_color,
+    )
+
+def generate_badge(left_text: str,
                    right_text: str,
-                   right_color,
+                   right_color: str,
                    link: Optional[str] = None,
-                   logo: Optional[str] = None,
                    left_color: str = '#555'
                    ) -> str:
     svg = badge(
@@ -14,11 +38,12 @@ def badge_generate(left_text: str,
         right_text = right_text,
         right_link = link,
         left_link = link,
-        logo = logo,
         left_color = left_color,
         right_color = right_color,
     )
-    if link is not None:
+    if link:
+        return svg
+    else:
         return svg.replace('xlink:', '')
-    return svg
 
+print(Badge("SSG", "43", "pink").image())
